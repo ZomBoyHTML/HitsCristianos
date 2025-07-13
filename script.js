c259d38115810c46c94ef7488a44bd9ae888042a
ZomBoy <106455717+ZomBoyHTML@users.noreply.github.com>
2025-07-13 09:33:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,641 @@
+const canciones = {
+    razon_cantar: {
+      titulo: "Mi razón de cantar",
+      artista: "Roxanna Contreras",
+      año: "1980",
+      youtubeId: "j4aga6vd3uM"
+    },
+    grande_amor: {
+      titulo: "Grande es tu amor",
+      artista: "Coros Unidos",
+      año: "2016",
+      youtubeId: "XY4nutdCACA"
+    },
+    mora_vida: {
+      titulo: "Mora en mi Vida",
+      artista: "Gladys Muñoz",
+      año: "2018",
+      youtubeId: "9hNC1I7YiEk"
+    },
+    ebenezer: {
+      titulo: "Ebenezer",
+      artista: "Coros Unidos",
+      año: "2013",
+      youtubeId: "dZRRRqxCweI"
+    },
+    amanecer_glorioso: {
+      titulo: "Amanecer Glorioso",
+      artista: "Coros Unidos",
+      año: "1987",
+      youtubeId: "_F01MPA4gKo"
+    },
+    a_dios_la_gloria: {
+      titulo: "A Dios sea la Gloria",
+      artista: "Elim Central",
+      año: "1991",
+      youtubeId: "JuAwV0Hkr8I"
+    },
+    dios_real: {
+      titulo: "Oh mi Dios Real",
+      artista: "Kenneth Morris",
+      año: "1930",
+      youtubeId: "lksiQzFuImA"
+    },
+    me_ha_tocado: {
+      titulo: "He Touched Me(Me ha tocado)",
+      artista: "Bill y Gloria Gaither",
+      año: "1963",
+      youtubeId: "HMO2pmxttZo"
+    },
+    por_ti_por_mi: {
+      titulo: "Por ti, por mí",
+      artista: "IMPCH",
+      año: "1990",
+      youtubeId: "bNDtFR3WFaE"
+    },
+    vive_hoy:{
+      titulo: "Because He Lives(Porque el vive hoy)",
+      artista: "William J. & Gloria Gaither",
+      año: "1971",
+      youtubeId: "oPW9xYEyijQ"
+    },
+    divino_companero: {
+      titulo: "Divino Compañero",
+      artista: "Antonio Rivera",
+      año: "1970",
+      youtubeId: "gR1NCY1gMio"
+    },
+    rey_esta_volviendo: {
+      titulo: "The King Is Coming(El Rey está Volviendo)",
+      artista: "Bill & Gloria Gaither",
+      año: "1983",
+      youtubeId: "H4IbRlbiK9g"
+    },
+    cielo_nuevo: {
+      titulo: "Hay Un Cielo Nuevo",
+      artista: "IMPCH",
+      año: "1950",
+      youtubeId: "U9cQF5OTe64"
+    },
+    te_alabo_dios:{
+      titulo: "Te Alabo oh Dios",
+      artista: "IMPCH",
+      año: "1958",
+      youtubeId: "7MnDyh8pYyw"
+    },
+    vendra_senor: {
+      titulo: "Vendrá el Señor",
+      artista: "IMPCH",
+      año: "1952",
+      youtubeId: "usgZ1COwyRg"
+    },
+    cuantas_veces: {
+      titulo: "Cuántas veces",
+      artista: "IMPCH",
+      año: "1955",
+      youtubeId: "TP2gZCEjc0A"
+    },
+    ayer_hoy:{
+      titulo: "Ayer y hoy",
+      artista: "IMPCH",
+      año: "1949",
+      youtubeId: "HcW9fP5eg2A"
+    },
+    permiteme_senor:{
+      titulo: "Permíteme, Señor",
+      artista: "IMPCH",
+      año: "1973",
+      youtubeId: "C3Mj4RKEtCM"
+    },
+    el_leproso:{
+      titulo: "El Leproso",
+      artista: "IMPCH",
+      año: "1975",
+      youtubeId: "01QVLzH47nY"
+    },
+    siento_gozo: {
+      titulo: "Hoy yo siento gozo",
+      artista: "IMPCH",
+      año: "1985",
+      youtubeId: "DjYDI7Nof8I"
+    },
+    porque_triste:{
+      titulo: "¿Por qué estás triste?",
+      artista: "IMPCH",
+      año: "1968",
+      youtubeId: "kcKuamGjvrs"
+    },
+    no_hay_problema:{
+      titulo: "No hay problema",
+      artista: "Danny Berríos",
+      año: "2005",
+      youtubeId: "z7wX7U5zhVw"
+    },
+    maravilloso_poder: {
+      titulo: "Maravilloso Poder",
+      artista: "IMPCH",
+      año: "1989",
+      youtubeId: "mML594oibv4"
+    },
+    manana_gloriosa: {
+      titulo: "Mañana Gloriosa",
+      artista: "Roberto C. Savage",
+      año: "1949",
+      youtubeId: "EBzlzP7cQ0g"
+    },
+    estas_preparado: {
+      titulo: "¿Estás tu Preparado?",
+      artista: "Pablo Pardo, Damaris Marín, Felipe Venegas",
+      año: "2009",
+      youtubeId: "tS0VDhTiFWo"
+    },
+    hoy_dia_vi:{
+      titulo: "Hoy día vi",
+      artista: "IMPCH",
+      año: "2013",
+      youtubeId: "hzxXgt61pVY"
+    },
+    me_siento_feliz: {
+      titulo: "Me siento feliz",
+      artista: "IMPCH",
+      año: "2012",
+      youtubeId: "hphE_rfzOmQ"
+    },
+    lindo_mirar:{
+      titulo: "Que Lindo, es su Mirar",
+      artista: "IMPCH",
+      año: "1974",
+      youtubeId: "pccAAa1JrqY"
+    },
+    que_piensas_tu:{
+      titulo: "¿Qué piensas tú?",
+      artista: "IMPCH",
+      año: "1994",
+      youtubeId: "f0LEJHKaFqQ"
+    },
+    inmensidad:{
+      titulo: "En La Inmensidad",
+      artista: "IMPCH",
+      año: "2004",
+      youtubeId: "6GxQTBKg4fU"
+    },
+    no_se_pq:{
+      titulo: "No sé por qué",
+      artista: "IMPCH",
+      año: "2000",
+      youtubeId: "clAg99HkmJA"
+    },
+    desciende:{
+      titulo: "Desciende",
+      artista: "IMPCH",
+      año: "1969",
+      youtubeId: "EOIqar3vX0E"
+    },
+    mire_jesus:{
+      titulo: "Mire a Jesús",
+      artista: "IMPCH",
+      año: "1988",
+      youtubeId: "tdL8IUKvYA4"
+    },
+    gloria_para_ti:{
+      titulo: "La Gloria es para ti",
+      artista: "IMPCH",
+      año: "1992",
+      youtubeId: "VTQouuw7yFs"
+    },
+    gratitud:{
+      titulo: "Gracias te doy a ti Señor",
+      artista: "IMPCH",
+      año: "1993",
+      youtubeId: "zWa85nD73_U"
+    },
+    uno:{
+      titulo: "Uno",
+      artista: "Coros Unidos",
+      año: "2009",
+      youtubeId: "jSsn32LtLUc"
+    },
+    creacion:{
+      titulo: "Creación",
+      artista: "IMPCH",
+      año: "2007",
+      youtubeId: "16qMyv7tBco"
+    },
+    vale_la_pena: {
+      titulo: "Si Vale la pena",
+      artista: "Coro Unidos",
+      año: "2001",
+      youtubeId: "TJLwao67HWE"
+    },
+    grande_eterno: {
+      titulo: "Grande y Eterno",
+      artista: "Coro Unidos",
+      año: "2009",
+      youtubeId: "rKzvCKHLrGc"
+    },
+    alegria_ser_de_cristo: {
+      titulo: "Que Alegría es ser de Cristo",
+      artista: "Coro Unidos",
+      año: "2007",
+      youtubeId: "ewiS0hlMpZg"
+    },
+    he_creido: {
+      titulo: "Yo he creido",
+      artista: "Coro Unidos",
+      año: "1978",
+      youtubeId: "sU9DhmYVfQQ"
+    },
+    gran_victoria: {
+      titulo: "La Gran Victoria",
+      artista: "Polo Negrete",
+      año: "2010",
+      youtubeId: "axZ3hJvO78g"
+    },
+    estoy_feliz: {
+      titulo: "Estoy Feliz",
+      artista: "Óscar Medina",
+      año: "1995",
+      youtubeId: "7mPKBwbGsus"
+    },
+    alaba_dios: {
+      titulo: "Alaba a Dios",
+      artista: "Danny Berríos",
+      año: "2001",
+      youtubeId: "r-5SZyal9eU"
+    },
+    templos_moradas:{
+      titulo: "Templos y Moradas",
+      artista: "Coros Unidos",
+      año: "2008",
+      youtubeId: "NRwk2VQBHJo"
+    },
+    consagrado:{
+      titulo: "Consagrado a Ti",
+      artista: "Coros Unidos",
+      año: "2010",
+      youtubeId: "W6_9KXd0KE8"
+    },
+    te_alabare:{
+      titulo: "Por Siempre Te Alabaré",
+      artista: "Horacio Vivares",
+      año: "2013",
+      youtubeId: "bAS7Fl4KxRk"
+    },
+    rayo_fugaz:{
+      titulo: "Rayo Fugaz",
+      artista: "Gladys Muñoz",
+      año: "2006",
+      youtubeId: "h9nuK7hBGfo"
+    },
+    bautizame:{
+      titulo: "Bautízame",
+      artista: "Tony Sauceda",
+      año: "1976",
+      youtubeId: "mobfbxgjmCg"
+    },
+    lucero:{
+      titulo: "Lucero de la madrugada",
+      artista: "IMPCH",
+      año: "1986",
+      youtubeId: "kM44gEBCXac"
+    },
+    lenguaje_fe: {
+      titulo: "Lenguaje de Fe",
+      artista: "IMPCH",
+      año: "1981",
+      youtubeId: "lz3i3C0GYJk"
+    },
+    yo_declaro: {
+      titulo: "Yo Declaro",
+      artista: "Danny Berríos",
+      año: "2012",
+      youtubeId: "xAuT31VA5Uc"
+    },
+    siempre_estuviste_aqui: {
+      titulo: "Siempre Estuviste Aquí",
+      artista: "Juan Carlos González",
+      año: "2014",
+      youtubeId: "GF5aP0hhhmM"
+    },
+    poder_nombre: {
+      titulo: "El Poder de Tu Nombre",
+      artista: "Coros Unidos",
+      año: "2009",
+      youtubeId: "YJZzMphxvPE"
+    },
+    su_sangre: {
+      titulo: "Su Sangre",
+      artista: "Coros Unidos",
+      año: "2015",
+      youtubeId: "rKmJZGQry4g"
+    },
+    toda_gloria:{
+      titulo: "Tú te Mereces Toda la Gloria",
+      artista: "Coros Unidos",
+      año: "2014",
+      youtubeId: "ENFCaml2F-I"
+    },
+    conmigo_va:{
+      titulo: "Conmigo va",
+      artista: "Coros Unidos",
+      año: "2018",
+      youtubeId: "TytGu-cJtho"
+    },
+    creele_a_dios: {
+      titulo: "Créele a Dios",
+      artista: "Los Hermanos Vargas",
+      año: "2021",
+      youtubeId: "1HHI6quzrRE"
+    },
+    yo_vencere: {
+      titulo: "Yo Venceré",
+      artista: "Coros Unidos",
+      año: "2012",
+      youtubeId: "i2oiFpUXzbY"
+    },
+    luz_y_salvacion: {
+      titulo: "El Señor es mi luz y mi salvación",
+      artista: "Rey David(salmo 27)",
+      año: "2010",
+      youtubeId: "nAHT4EPvN8E"
+    },
+    te_levantare: {
+      titulo: "Te Levantaré",
+      artista: "Carlos Leiva",
+      año: "2009",
+      youtubeId: "spXB5v0ylrY"
+    },
+    iglesia:{
+      titulo: "Iglesia se acerca la hora",
+      artista: "Polo Negrete",
+      año: "2011",
+      youtubeId: "3GT5Z_p_JIE"
+    },
+    rey_del_universo: {
+      titulo: "Rey del Universo",
+      artista: "IMPCH",
+      año: "2014",
+      youtubeId: "zZL7Em3vTqY"
+    },
+    siempre_camine:{
+      titulo: "Siempre Caminé",
+      artista: "Coros Unidos",
+      año: "2015",
+      youtubeId: "V44-ZEcB3Q8"
+    },
+    mi_redentor: {
+      titulo: "Mi Redentor",
+      artista: "Coros Unidos",
+      año: "2012",
+      youtubeId: "BWk-3MwK-RY"
+    },
+    eterno_redentor: {
+      titulo: "Eterno Redentor",
+      artista: "Coros Unidos",
+      año: "2023",
+      youtubeId: "XAK-4rcRiXU"
+    },
+    confianza:{
+      titulo: "Ten Confianza",
+      artista: "Coros Unidos",
+      año: "2012",
+      youtubeId: "QV8PQBj1iJk"
+    },
+    vive:{
+      titulo: "me dijo Vive",
+      artista: "Coros Unidos",
+      año: "2013",
+      youtubeId: "eSmqQuUrYuQ"
+    },
+    tu_presencia: {
+      titulo: "Tu Presencia señor",
+      artista: "Coros Unidos",
+      año: "2020",
+      youtubeId: "qjpOlA-wqXA"
+    },
+    razon_vida: {
+      titulo: "Razón de mi vida",
+      artista: "Gladys Muñoz",
+      año: "2018",
+      youtubeId: "3SPpKYdc3MQ"
+    },
+    tu_bandera:{
+      titulo: "Tu Bandera",
+      artista: "Jesús Adrián Romero",
+      año: "2010",
+      youtubeId: "2h89c_ESuAY"
+    },
+    maravilloso_evento: {
+      titulo: "Maravilloso Evento",
+      artista: "Coros Unidos",
+      año: "2015",
+      youtubeId: "H9mjfbwiDN8"
+    },
+    gloria_jehová: {
+      titulo: "la Gloria de Jehová",
+      artista: "Coros Unidos",
+      año: "1982",
+      youtubeId: "QmM7_7UP0PM"
+    },
+    proclamare_victoria: {
+      titulo: "Proclamaré Victoria",
+      artista: "Coros Unidos",
+      año: "2008",
+      youtubeId: "SMdryca4Abg"
+    },
+    digno_santo:{
+      titulo: "Digno y Santo",
+      artista: "Coros Unidos",
+      año: "1997",
+      youtubeId: "2qcD3dpWBXg"
+    },
+    carta_timoteo:{
+      titulo: "Carta a Timoteo",
+      artista: "LOS EVANGELISTAS",
+      año: "2007",
+      youtubeId: "BA7k0RKJlb4"
+    },
+    sin_manchas:{
+      titulo: "Sin Manchas y sin arrugas",
+      artista: "Ubrano Arroyo",
+      año: "1976",
+      youtubeId: "pO5TbN_JlRU"
+    },
+    josue:{
+      titulo: "Josué 10:12",
+      artista: "Samuel Santos",
+      año: "2012",
+      youtubeId: "aRJDLxuTFTY"
+    },
+    siento_senor: {
+      titulo: "Siento Señor",
+      artista: "Coros Unidos",
+      año: "2003",
+      youtubeId: "vbACi_x94aI"
+    },
+    soy_milagro:{
+      titulo: "Sou Um Milagre(Soy Milagro)",
+      artista: "Carlos Alberto Moyses",
+      año: "1980",
+      youtubeId: "isMrX5js9og"
+    },
+    me_alegrare:{
+      titulo: "Todavia Me alegrarei(Todavía Me alegraré)",
+      artista: "Samuel Messias",
+      año: "2016",
+      youtubeId: "rNBedSCKwVc"
+    },
+    escudo:{
+      titulo: "Escudo",
+      artista: "Ariel Villareal",
+      año: "2012",
+      youtubeId: "oaqkqx-4veQ"
+    },
+    plenitud:{
+      titulo: "Mi Plenitud",
+      artista: "Franco Figueroa",
+      año: "2018",
+      youtubeId: "l4nT560iuqk"
+    },
+    cristo_yo_creo:{
+      titulo: "Cristo, yo creo en ti",
+      artista: "Crystal Lewis",
+      año: "2011",
+      youtubeId: "g-4mbgboRm0"
+    },
+    salon_trono:{
+      titulo: "The Throne Room Song(Salón del Trono)",
+      artista: "People & Songs",
+      año: "2004",
+      youtubeId: "1E-KdAED9o0"
+    },
+    rey_mesias: {
+      titulo: "Rey Mesías",
+      artista: "Coros Unidos",
+      año: "2023",
+      youtubeId: "LIsubjlEczU"
+    },
+    tumbas_jardines:{
+      titulo: "Tumbas a Jardines",
+      artista: "Brandon Lake",
+      año: "2020",
+      youtubeId: "ooGwRE1gmD8"
+    },
+    santo_siempre:{
+      titulo: " Holy Forever(Santo por siempre)",
+      artista: "Chris Tomlin",
+      año: "2022",
+      youtubeId: "VhdAZ2aQtBk"
+    },
+    conocer_jesus: {
+      titulo: "Quiero Conocer a Jesús",
+      artista: "Coros Unidos",
+      año: "2020",
+      youtubeId: "Pn-PqlB_K4M"
+    },
+    lugar_alto:{
+      titulo: "No hay Lugar mas Alto",
+      artista: "Miel San Marcos, Sammy Morales",
+      año: "2015",
+      youtubeId: "9FZiGbiwIo0"
+    },
+    tiernas_canciones: {
+      titulo: "Tiernas Canciones",
+      artista: "Eliza E. Hewitt",
+      año: "1852",
+      youtubeId: "NqasDIcjFC0"
+    },
+    sin_condicion:{
+      titulo: "Amor Sin Condición",
+      artista: "Cory Asbury",
+      año: "2017",
+      youtubeId: "6xx0d3R2LoU"
+    },
+    abra_cielo: {
+      titulo: "Que se Abra el Cielo",
+      artista: "Christine D'Clario",
+      año: "2015",
+      youtubeId: "ATeYsoq7TFg"
+    },
+    yahweh:{
+      titulo: "Yahweh Se Manifestará",
+      artista: "Oasis Ministry",
+      año: "2017",
+      youtubeId: "kHp8Rww7Uek"
+    },
+    santo_vive: {
+      titulo: "Santo es el que Vive",
+      artista: "Montesanto ",
+      año: "2023",
+      youtubeId: "y9Hp_WN6lHk"
+    },
+    te_doy_gracias: {
+      titulo: "Te doy gracias",
+      artista: "LATIDOS",
+      año: "2010",
+      youtubeId: "bD6h1S8LID8"
+    },
+    vista:{
+      titulo: "Vista Espiritual",
+      artista: "Coros Unidos",
+      año: "2015",
+      youtubeId: "9fdxAtoMqoA"
+    },
+    sin_ti:{
+      titulo: "Sin Ti Señor",
+      artista: "Coros Unidos",
+      año: "2001",
+      youtubeId: "fI9e2Ant6lY"
+    },
+    cordero:{
+      titulo: "El Cordero inmolado",
+      artista: "Achtliederbuch",
+      año: "1524",
+      youtubeId: "0vyfYhO9tqU"
+    },
+    cuarto_varon:{
+      titulo: "El Cuarto Varón",
+      artista: "Fanny Narváez",
+      año: "2010",
+      youtubeId: "1G9R7KS86-8"
+    }
+
+
+  };
+  console.log("Cantidad de alabanzas:", Object.keys(canciones).length);
+
+  function onScanSuccess(decodedText) {
+    if (canciones[decodedText]) {
+      const cancion = canciones[decodedText];
+      document.getElementById("tituloCancion").textContent = cancion.titulo;
+      document.getElementById("detalleCancion").textContent = `${cancion.artista} - ${cancion.año}`;
+      document.getElementById("youtubePlayer").src =
+        `https://www.youtube.com/embed/${cancion.youtubeId}?autoplay=1&rel=0`;
+    } else {
+      document.getElementById("tituloCancion").textContent = "Código QR no reconocido";
+      document.getElementById("detalleCancion").textContent = "";
+      document.getElementById("youtubePlayer").src = "";
+    }
+  }
+
+  const html5QrCode = new Html5Qrcode("reader");
+  Html5Qrcode.getCameras()
+    .then((devices) => {
+      if (devices && devices.length) {
+        const cameraId = devices[0].id;
+        html5QrCode.start(
+          cameraId,
+          {
+            fps: 10,
+            qrbox: 250,
+            aspectRatio: 1.0
+          },
+          onScanSuccess
+        );
+      } else {
+        alert("No se encontró cámara disponible.");
+      }
+    })
+    .catch((err) => {
+      alert("Error al acceder a la cámara: " + err);
+    });
